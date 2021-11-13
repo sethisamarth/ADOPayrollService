69a9471433b5f564167f9e9cae31c56a404fdeaa
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-11-13 23:19:40 +0530

    RefactorCode_SelectQueryBasedonName

--- ADO_Employee_Payroll/ADO_Employee_Payroll/Program.cs
+++ ADO_Employee_Payroll/ADO_Employee_Payroll/Program.cs
@@ -9,8 +9,15 @@ namespace ADO_Employee_Payroll
             Console.WriteLine("Welcome to Payroll Services using ADO!");
             //Create oobject for Employee Repository
             EmployeeRepository employeeRepository = new EmployeeRepository();
+            EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+            employeeDataManager.EmployeeID = 2;
+            employeeDataManager.EmployeeName = "shivam";
+            employeeDataManager.BasicPay = 3000000;
             employeeRepository.GetSqlData();
             employeeRepository.UpdateSalaryQuery();
+            employeeRepository.UpdateSalary(employeeDataManager);
+          
+            
         }
     }
 }
